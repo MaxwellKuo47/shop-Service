440e1ff820be923bdfa48b360df8f0ea8cfee222
郭冠景 <maxwellkuo47@guoguanjingdeMacBook-Pro.local>
2022-12-29 00:40:57 +0800

    image upload

--- cmd/web/productHandler.go
+++ cmd/web/productHandler.go
@@ -1,9 +1,71 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"sync"
+)
 
-const MAX_FORM_ACCEPT = 10 * 1024 * 1024
+const MAX_FORM_ACCEPT = 10 << 20 //10MB
+const PRODUCT_IMAGE_FOLDER  = "/home/workspace/code/ReactPractice/Image/"
+
+var wg sync.WaitGroup
 
 func (app *AppConfig) productAdd (w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(MAX_FORM_ACCEPT) //10MB
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	err := r.ParseMultipartForm(MAX_FORM_ACCEPT) 
+	if err != nil {
+		app.clientError(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Println(r.PostForm.Get("info"))
+	wg.Add(2)
+	go func (size string) {
+		for i := 0; i < 5; i++ {
+			num := strconv.Itoa(i)
+			file, _, err := r.FormFile("file"+ size +num)
+			fmt.Println("file"+num)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+			body, err := ioutil.ReadAll(file)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+			err = ioutil.WriteFile("./file"+ size +num+".jpeg", body, 0644)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+		}
+		wg.Done()
+	}("Lg")
+	go func (size string) {
+		for i := 0; i < 5; i++ {
+			num := strconv.Itoa(i)
+			file, _, err := r.FormFile("file"+ size +num)
+			fmt.Println("file"+num)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+			body, err := ioutil.ReadAll(file)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+			err = ioutil.WriteFile("./file"+ size +num+".jpeg", body, 0644)
+			if err != nil {
+				app.clientError(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+		}
+		wg.Done()
+	}("Sm")
+	wg.Wait()
 } 
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,7 +10,7 @@ func (app *AppConfig) routes() *httprouter.Router {
 	httpRouter := httprouter.New()
 	// httpRouter.Handler(http.MethodGet, "/page",http.StripPrefix("/page/",http.FileServer(http.Dir("./build"))))
 	httpRouter.HandlerFunc(http.MethodGet, "/api/ping", app.requestLogger(app.ping))
-	httpRouter.HandlerFunc(http.MethodPost, "/api/product/add", app.requestLogger(app.productAdd))
+	httpRouter.HandlerFunc(http.MethodPost, "/api/product", app.requestLogger(app.productAdd))
 
 	return httpRouter
 }
